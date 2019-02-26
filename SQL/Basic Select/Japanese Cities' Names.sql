98645c9b7691847a97e834957a3f069a0bc0831a
Jamshed Ahmedov <jamsheda@silkroadprofessionals.com>
2019-02-26 23:51:33 +0500

    Solved some Basic Select

---
+++ SQL/Basic Select/Japanese Cities' Names.sql
@@ -0,0 +1,4 @@
+--Problem:      https://www.hackerrank.com/challenges/japanese-cities-name/problem
+--Max Score:    10
+
+SELECT Name FROM City WHERE CountryCode = 'JPN'
---
+++ SQL/Basic Select/Weather Observation Station 1.sql
@@ -0,0 +1,5 @@
+--Problem:     https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+--Max Score:   15
+
+SELECT City, State 
+FROM STATION  
